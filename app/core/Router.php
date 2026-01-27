4897642e2f5489ef2f0937b4e44e52840b42ebc2
nikoe-ee <nicholas.tantan2009@gmail.com>
2026-01-27 08:05:58 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -6,7 +6,17 @@ class Router
 
     public function run()
     {
-        echo "Router is running";
+       $method = $_SERVER['REQUEST_METHOD'];
+       $uri = parse_url($_SERVER['REQUEST_URI'], component: PHP_URL_PATH);
+
+       if ($method = 'GET' && $uri == '/students') {
+              echo '<h1>Menampilkan daftar siswa</h1>';
+              return;
+       }
+
+          http_response_code(404);
+    echo '<h1>Page Not Found</h1>';
+
     }
 
 }
