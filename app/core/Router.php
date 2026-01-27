8b1b30eebef63a738acafde3a284bb1a0ccc89a5
nikoe-ee <nicholas.tantan2009@gmail.com>
2026-01-27 08:34:42 +0700

    feat: implement student controller

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Core;
 
+use App\Controllers\StudentController;
+
 class Router
 {
 
@@ -9,13 +11,23 @@ class Router
        $method = $_SERVER['REQUEST_METHOD'];
        $uri = parse_url($_SERVER['REQUEST_URI'], component: PHP_URL_PATH);
 
-       if ($method = 'GET' && $uri == '/students') {
-              echo '<h1>Menampilkan daftar siswa</h1>';
+       if ($method == 'GET' && $uri == '/students') {
+            require_once './app/controllers/StudentController.php';
+             $controllers = new StudentController();
+             $controllers->index();
               return;
+
+            
        }
 
+           if ($method == 'GET' && $uri == '/students/create') {
+            require_once './app/controllers/StudentController.php';
+              $controllers = new StudentController();
+             $controllers->create();
+              return; }
+
           http_response_code(404);
-    echo '<h1>Page Not Found</h1>';
+            echo '<h1>Page Not Found</h1>';
 
     }
 
