c2dc1fc2338f85ecfa09a9bbc67767ca7e8596bf
nikoe-ee <nicholas.tantan2009@gmail.com>
2026-04-10 10:56:01 +0700

    feat: implement layout

--- app/views/students/show.php
+++ app/views/students/show.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Siswa</title>
-    <link rel="stylesheet" href="/css/output.css">
-</head>
-
-<body class="min-h-screen flex flex-col bg-gray-100">
-
-    <!--- Header Start --->
-
-    <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-4">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
-        </div>
-    </header>
-
-    <!--- Header End --->
-
-    <!--- Main Start --->
-
-    <main class="grow container mx-auto">
-        <div class="mt-8 space-y-4">
+     <div class="mt-8 space-y-4">
             <!--- Card Header Start --->
             <div class="bg-white shadow rounded-lg p-4">
                 <h1 class="text-2xl font-bold">Detail Siswa</h1>
@@ -61,22 +35,3 @@
             <!--- Card Content End --->
 
         </div>
-
-    </main>
-
-    <!--- Main End --->
-
-    <!--- Footer Start --->
-
-    <footer class="bg-gray-800 text-white">
-        <div class="text-center p-4">
-            &copy
-            <?= date(format: "Y") ?> Sistem Sekolah SMK Kristen Immanuel
-        </div>
-    </footer>
-
-    <!--- Footer End --->
-
-</body>
-
-</html>
